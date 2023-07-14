f88858e4f066cd4ba80a0869f313a6de37e9f648
adijdhv <aadeshjadhav14@mgmail.com>
2023-07-15 00:07:13 +0530

    changes

--- functions/app.js
+++ functions/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const cors = require('cors')
-require("dotenv").config({path : "Backend/config/config.env"})
+require("dotenv").config({path : "functions/config/config.env"})
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
